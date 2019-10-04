6e9c0ad5ec05ef8bce0a979040dbef5d85aa48bc
Samuel LLOYD <samuel.lloyd@amadeus.com>
2019-10-04 09:25:57 -0400

    working on server

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname,'index.html'));
 });
-console.log("Server running on port "+port)
-app.listen(port);
+app.listen(port, function(){
+    console.log("Server active on port "+port)
+});
