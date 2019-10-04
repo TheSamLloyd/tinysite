769bf63b40e5bef7d618ea26d67128cb4b5a8525
Samuel LLOYD <samuel.lloyd@amadeus.com>
2019-10-04 09:19:04 -0400

    ibid

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/ping', function (req, res) {
   return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile('/index.html');
+  res.sendFile('index.html');
 });
 console.log("Server running on port "+port)
 app.listen(port);
